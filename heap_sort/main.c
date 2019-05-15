4d649d19644583f5693fc0a11587586870155919
aumujun <aumujun@gmail.com>
2019-05-15 17:43:56 +0800

    :wrench: reconstruction the heap sort

--- heap_sort/main.c
+++ heap_sort/main.c
@@ -3,63 +3,89 @@
 
 void heapSort(int* array, const int n);
 void heapAdjust(int* array, int n, int i);
-void swap(int* a, int* b);
 void printArray(int *array, const int n);
+void swap(int *array, const int i, const int j);
 
 int main()
 {
-	int array[] = {32, 0, 1, 0, 43, 99, 100, 0, 4, 9, 2, 1, 4, 9, 5, 3, 4};
+	int array[] = {12, 9, 4, 48, 9, 6};
 
 	int n = sizeof(array) / sizeof(array[0]);
 
+	printArray(array, n);
+	
 	heapSort(array, n);
 
+	printf("Sorted array is: \n");
 	printArray(array, n);
+
 	
 
 	return 0;
 }
 
-
-void swap(int* a, int* b)
+void swap(int *array, const int i, const int j)
 {
-	*a = *a ^ *b;
-	*b = *a ^ *b;
-	*a = *a ^ *b;
+	int temp;
+	temp = array[i];
+	array[i] = array[j];
+	array[j] = temp;
 }
 
 
+
+
 void heapAdjust(int* array, int n, int i)
+/* { */
+	/* int largest = i; */
+	/* int left = 2 * i + 1; */
+	/* int right = 2 * i + 2; */
+/*  */
+	/* if ( left < n && array[largest] < array[left] ) */
+	/* { */
+		/* largest = left; */
+	/* } */
+	/* if ( right < n && array[largest] < array[right] ) */
+	/* { */
+		/* largest = right; */
+/*  */
+
+	/* } */
+	/* if ( largest != i ) */
+	/* { */
+		/* swap(&array[i], &array[largest]); */
+		/* heapAdjust(array, n, largest); */
+	/* } */
+/* } */
 {
 	int largest = i;
-	int left = 2 * i + 1;
-	int right = 2 * i + 2;
+	int l = 2 * i + 1;
+	int r = 2 * i + 2;
 
-	if ( left < n && array[i] < array[left] )
+	if ( l < n && array[l] > array[largest] )
 	{
-		largest = left;
+		largest = l;
 	}
-	if ( right < n && array[largest] < array[right] )
+	if ( r < n && array[r] > array[largest] )
 	{
-		largest = right;
-
+		largest = r;
 	}
 	if ( largest != i )
 	{
-		swap(&array[i], &array[largest]);
+		swap(array, i, largest);
 		heapAdjust(array, n, largest);
 	}
 }
 
 void heapSort(int* array, const int n)
 {
-	for( int i=n; i>=0; i-- )
+	for( int i=n/2-1; i>=0; i-- )
 	{
 		heapAdjust(array, n, i);
 	}
-	for( int i=n-1; i>=0; i-- )
+	for( int i=n-1;i>=0; i-- )
 	{
-		swap(&array[i], &array[0]);
+		swap(array, 0, i);
 		heapAdjust(array, i, 0);
 	}
 }
@@ -67,8 +93,9 @@ void heapSort(int* array, const int n)
 
 void printArray(int* array, const int n)
 {
-	for( int i=0; i<n; i++ )
+	for( int i=0; i<n; ++i )
 	{
 		printf("%d ", array[i]);
 	}
+	printf("\n");
 }
