d829a891fd42cd0463fb5554436413d7e7889964
aumujun <aumujun@gmail.com>
2019-06-01 10:21:00 +0800

    learning pthread

---
+++ Thread/Two_join_detach/main.c
@@ -0,0 +1,67 @@
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <unistd.h>
+
+#define NUM_THREADS 4
+
+void *BusyWork(void *t);
+
+
+int main()
+{
+	pthread_t thread[NUM_THREADS];
+	pthread_attr_t attr;
+	int rc;
+	long t;
+	void *status;
+	pthread_attr_init(&attr);
+	pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE); // set thread to joinable
+	for ( t=0; t < NUM_THREADS; ++t )
+	{
+		printf("Main: creating thread %ld\n", t);
+		rc = pthread_create(&thread[t], &attr, BusyWork, &t);
+		if ( rc )
+		{
+			printf("ERROR; return code from pthread_create() is %d\n", rc);
+			exit(-1);
+		}
+	}
+	printf("==========================\n");
+	pthread_attr_destroy(&attr);
+	for ( t = 0; t < NUM_THREADS; ++t )
+	{
+		// prevent zombie threads
+		rc = pthread_join(thread[t], &status);
+		if ( rc ) 
+		{
+			printf("ERROR; return code from pthread_join() is %d\n", rc);
+			exit(-1);
+		}
+		printf("Main: completed join with thread %ld having a status of %ld\n", t, (long)status);
+		
+	}
+	printf("Main: program completed. Exiting. \n");
+	pthread_exit(NULL);
+
+
+	return 0;
+}
+
+
+
+void *BusyWork(void *t)
+{
+	int i;
+	long tid;
+	double result = 0.0;
+	tid = *(long *)t;
+	printf("Thread %ld starting...\n", tid);
+	for (i = 0; i < 1000000; ++i)
+	{
+		result = result + sin(i) * tan(i);
+	}
+	printf("Thread %ld done. Result = %e\n", tid, result);
+	pthread_exit((void*) t);
+}
