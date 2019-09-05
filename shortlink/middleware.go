bd4cd4817fcbc690ef884791fc0a1d131ffa1d04
0x1un <aumujun@gmail.com>
2019-09-05 18:00:24 +0800

    middleware

---
+++ shortlink/middleware.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"time"
+)
+
+type Middleware struct{}
+
+func (m *middleware) LoggingHandler(next http.Handler) http.Handler {
+	fn := func(w http.ResponseWriter, r *http.Request) {
+		t1 := time.Now()
+		next.ServeHTTP(w, r)
+		t2 := time.Now()
+		log.Printf("[%s] %q %v", r.Method, r.URL.String(), t2.Sub(t1))
+	}
+}
